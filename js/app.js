a7f3b4a85fa5237f54afebb2069ab8f52cf18895
Victor Aranguren <victoraranguren.dev@gmail.com>
2021-01-23 22:03:34 -0400

    El Switchbutton creado y corriendo de modo basico

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const btnToggle = document.querySelector(".toggle-btn");
+const ball = document.querySelector(".toggle-ball");
+
+const switchTheme = () => {
+  btnToggle.classList.toggle('active')
+  return console.log('Done')
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  btnToggle.addEventListener('click', switchTheme)
+});

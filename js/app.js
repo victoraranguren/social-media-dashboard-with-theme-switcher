efc979f35d0e336cdb461fcda69f15821aff6d44
Victor Aranguren <victoraranguren.dev@gmail.com>
2021-01-28 20:36:30 -0400

    dark togglebtn listo

--- js/app.js
+++ js/app.js
@@ -2,10 +2,11 @@ const btnToggle = document.querySelector(".toggle-btn");
 const ball = document.querySelector(".toggle-ball");
 
 const switchTheme = () => {
-  btnToggle.classList.toggle('active')
-  return console.log('Done')
+  btnToggle.classList.toggle("active");
+  ball.classList.toggle("active");
+  return console.log("Done");
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  btnToggle.addEventListener('click', switchTheme)
+  btnToggle.addEventListener("click", switchTheme);
 });

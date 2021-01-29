84d28249e6c0059dca56b777ed07afbaa6153df4
Victor Aranguren <victoraranguren.dev@gmail.com>
2021-01-28 22:41:07 -0400

    Completado dark mode

--- js/app.js
+++ js/app.js
@@ -2,9 +2,31 @@ const btnToggle = document.querySelector(".toggle-btn");
 const ball = document.querySelector(".toggle-ball");
 
 const switchTheme = () => {
+  const classArray = [
+    ".header",
+    ".header .container",
+    ".title-lg",
+    ".title",
+    ".title-sm",
+    ".toggle-text",
+    'body'
+  ];
+  const classArrayAll = [
+    ".name",
+    '.text',
+    '.followers',
+    '.number',
+    '.counter',
+    '.overview-card'
+  ];
+  classArray.forEach((el) => {
+    document.querySelector(`${el}`).classList.toggle("dark");
+  });
+  classArrayAll.forEach((el) => {
+    document.querySelectorAll(`${el}`).forEach(el=>el ? el.classList.toggle("dark") : false);
+  });
   btnToggle.classList.toggle("active");
   ball.classList.toggle("active");
-  return console.log("Done");
 };
 
 document.addEventListener("DOMContentLoaded", () => {
